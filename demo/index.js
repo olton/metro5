71545ed72c65b7143b403595dcbe68f4bf31b706
olton <serhii@pimenov.com.ua>
2023-03-10 12:18:07 +0200

    upd mif

--- demo/index.js
+++ demo/index.js
@@ -9,7 +9,6 @@ import {Metro5} from "../src/core/metro"
 import "../icons/mif.css"
 import * as CommonCss from "../src/common"
 import * as Components from "../src/components"
-import {Validator} from "../src/components";
 
 globalThis.Metro = new Metro5({
     removeCloakTimeout: 1000,
